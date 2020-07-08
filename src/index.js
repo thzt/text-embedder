bab4a5401bfc3104d180c3828847a45efb2678cb
thzt <thzt.xx@gmail.com>
2020-07-08 17:02:35 +0800

    feat: 统一依赖 line-embedder

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
-const log = require('./util/log');
+const lineEmbedder = require('line-embedder');
+const log = require('./log');
 
-const sort = require('./sort');
-const embed = require('./embed');
-
-/*
-  向文本中嵌入内容
-
-  config: {text, embeds: [{insert, replace, content}]}
-
-    file:      原始文本
-    insert:    在当前行前面插入 content
-    replace:   把多行替换为 content
-    content:   嵌入的内容
-
-  insert 与 replace 优先使用 insert
-*/
-const embedder = config => {
+/**
+ * 向文本中嵌入内容
+ * 
+ * @param text 原始文本
+ * @param embeds [{insert,replace,content}] 
+ * + insert 在当前行前面插入 content  
+ * + replace 把多行替换为 content  
+ * + content 嵌入的内容  
+ * 
+ * insert 与 replace 优先使用 insert
+ * 
+ * @return 修改后的文本
+ */
+const main = ({ text, embeds }, newline = '\n') => {
   log('开始执行');
-  const { text, embeds } = config;
 
-  log('排序并校验各个修改位置');
-  const sortedEmbeds = sort(embeds);
+  const lines = text.split(newline);
+  lineEmbedder({
+    lines,
+    embeds: embeds.map(({ insert, replace, content }) => ({
+      insert,
+      replace,
+      line: content,
+    })),
+    beforeEmbed: ({ mode, args }) => {
+      if (mode === 'insert') {
+        const insert = args;
+        log('嵌入内容，在第 %s 行之前', insert);
+        return;
+      }
 
-  log('做出修改');
-  const modifiedText = embed(text, sortedEmbeds);
+      // replace mode
+      const [from, to] = args;
+      log('替换内容，从第 %s 行到第 %s 行', from, to);
+    },
+  });
+
+  const modifiedText = lines.join(newline);
 
   log('执行完毕');
   return modifiedText;
 };
 
-module.exports = embedder;
+module.exports = main;
--- src/log.js
+++ src/log.js
@@ -1,5 +1,5 @@
 const debug = require('debug');
-const packageJson = require('../../package.json');
+const packageJson = require('../package.json');
 
 // 指定 debug 的前缀为 package.json 的 name 字段
 const log = debug(packageJson.name);
